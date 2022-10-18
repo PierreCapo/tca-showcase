3c9b706254850d9939acd65731aef60eea8e0cb7
PierreCapo <pierre.caporossi@gadz.org>
2022-10-18 09:39:00 +0200

    Initial Commit

---
+++ pres/presApp.swift
@@ -0,0 +1,17 @@
+//
+//  presApp.swift
+//  pres
+//
+//  Created by Pierre Caporossi on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct presApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ presUITests/presUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  presUITestsLaunchTests.swift
+//  presUITests
+//
+//  Created by Pierre Caporossi on 18/10/2022.
+//
+
+import XCTest
+
+final class presUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
